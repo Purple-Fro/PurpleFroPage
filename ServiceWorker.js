e25585413c0ac39f2b09e4ad6afe7fa4131718b8
Negawisp <BlindEyed@yandex.ru>
2025-05-14 14:02:21 +0300

    0.8.0
    - Casino animations

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,5 +1,5 @@
 var isCacheCleared = false;
-const cacheName = "GameSolutions-PurpleFro-0.7.0";
+const cacheName = "GameSolutions-PurpleFro-0.8.0";
 const contentToCache = [
     "Build/PurpleFroPage.loader.js",
     "Build/PurpleFroPage.framework.js.unityweb",
