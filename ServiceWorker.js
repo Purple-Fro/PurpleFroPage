68b43922088185aec8c77f02e7a4ac56a9204d61
Negawisp <BlindEyed@yandex.ru>
2025-06-11 11:27:31 +0300

    0.12.0
    - Fullscreen

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,5 +1,5 @@
 var isCacheCleared = false;
-const cacheName = "GameSolutions-PurpleFro-0.11.0";
+const cacheName = "GameSolutions-PurpleFro-0.12.0";
 const contentToCache = [
     "Build/PurpleFro.loader.js",
     "Build/PurpleFro.framework.js.unityweb",
