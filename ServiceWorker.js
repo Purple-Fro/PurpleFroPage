00b6cdcbb31f4f86e506d9af1ecec1763c9a33eb
Negawisp <BlindEyed@yandex.ru>
2025-06-11 14:13:40 +0300

    0.14.0
    - Casino saves

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,5 +1,5 @@
 var isCacheCleared = false;
-const cacheName = "GameSolutions-PurpleFro-0.13.0";
+const cacheName = "GameSolutions-PurpleFro-0.14.0";
 const contentToCache = [
     "Build/PurpleFro.loader.js",
     "Build/PurpleFro.framework.js.unityweb",
